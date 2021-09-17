1c41a190117748ecf713c11c80306c7c10304fea
Abhishek Saxena <saxenaa218@gmail.com>
2021-09-17 17:27:22 +0530

    feat(scheduler): scheduler UI with form submissions

--- src/scheduler/Schedule.jsx
+++ src/scheduler/Schedule.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import cronstrue from 'cronstrue'
-import { Form, Input, Select, DatePicker, Radio, TimePicker, Alert } from "antd";
+import moment from 'moment'
+import { Form, Input, Select, DatePicker, Radio, TimePicker, Alert, Row, Col, Button } from "antd";
 
 const formItemLayout = {
   labelCol: {
@@ -23,18 +24,28 @@ const Schedule = (props) => {
   const modules = ["Datalayer", "Do You Know"];
   const requestMethods = ["Get", "Post"];
   const authRequired = ["Yes", "No"];
+  const dateFormat = 'DD-MM-YYYY';
+  const timeFormat = 'hh:mm:ss';
 
   const handleSubmit = (e) => {
+    e.preventDefault();
     validateFieldsAndScroll((err, values) => {
       if (!err) {
-        console.log(values);
+        if (values.hasOwnProperty('cronExpression')) {
+          const result = { ...values };
+          result.cronExpression = cronExpression.join(" ");
+          result.cronNarration = cronstrue.toString(cronExpression.join(" "), { dayOfWeekStartIndexZero: false })
+          console.log(result);
+        }
+        else {
+          console.log(values);
+        }
       }
     });
   };
 
   const handleOnChange = (e) => {
-    console.log('here', e.target.value)
-    if (e.target.value === "g") {
+    if (e.target.value === "Custom cron") {
       setVisible(true);
     }
   };
@@ -44,7 +55,7 @@ const Schedule = (props) => {
   }
 
   return (
-    <div style={{ width: "90%", marginTop: "20px" }}>
+    <div style={{ width: "90%", margin: "20px" }}>
       <Form onSubmit={handleSubmit} {...formItemLayout}>
         <Form.Item label="Scheduler name">
           {getFieldDecorator("name", {
@@ -53,11 +64,12 @@ const Schedule = (props) => {
         </Form.Item>
         <Form.Item label="Description">
           {getFieldDecorator("description", {
-            rules: [{ required: true, message: "Enter description" }],
+            rules: [{ required: false, message: "Enter description" }],
           })(<Input placeholder="Enter description" />)}
         </Form.Item>
         <Form.Item label="Module">
           {getFieldDecorator("module", {
+            initialValue: modules[0],
             rules: [{ required: true, message: "Please input your Password!" }],
           })(
             <Select>
@@ -76,73 +88,109 @@ const Schedule = (props) => {
         </Form.Item>
         <Form.Item label="Frequency">
           {getFieldDecorator("frequency", {
-            initialValue: 'a',
+            initialValue: 'Daily',
             rules: [{ required: true, message: "Please input your Password!" }],
           })(
             <Radio.Group 
               onChange={handleOnChange}
             >
-              <Radio value="a">Daily</Radio>
-              <Radio value="b">Weekly</Radio>
-              <Radio value="c">Monthly</Radio>
-              <Radio value="d">Yearly</Radio>
-              <Radio value="e">Custom date</Radio>
-              <Radio value="g" onClick={handleClick} >
-                Custom Cron
+              <Radio value="Daily">Daily</Radio>
+              <Radio value="Weekly">Weekly</Radio>
+              <Radio value="Monthly">Monthly</Radio>
+              <Radio value="Yearly">Yearly</Radio>
+              <Radio value="Custom date">Custom date</Radio>
+              <Radio value="Custom cron" onClick={handleClick} >
+                Custom cron
               </Radio>
             </Radio.Group>
           )}
         </Form.Item>
-        {['a', 'b', 'c', 'd'].includes(getFieldValue("frequency")) && (
-          <div style={{ display: 'flex', justifyContent: 'flex-end' }}>
-            <Form.Item label="Start date" labelCol={{ span: 24 }} wrapperCol={{ span: 24 }}>
-              {getFieldDecorator('startDate', {
-                rules: [{ required: true, message: 'Please select your Start date!' }],
-              })(
-                <DatePicker style={{ width: '150px' }} />,
-              )}
-            </Form.Item>
-            <Form.Item label="End date" labelCol={{ span: 24 }} wrapperCol={{ span: 24 }}>
-              {getFieldDecorator('endDate', {
-                rules: [{ required: true, message: 'Please select your End date!' }],
-              })(
-                <DatePicker style={{ width: '150px' }} />,
-              )}
-            </Form.Item>
-            <Form.Item label="Select time" labelCol={{ span: 24 }} wrapperCol={{ span: 24 }}>
-              {getFieldDecorator('time', {
-                rules: [{ required: true, message: 'Please select your time!' }],
-              })(
-                <TimePicker style={{ width: '150px' }} />,
-              )}
-            </Form.Item>
-          </div>
+        {['Daily', 'Weekly', 'Monthly', 'Yearly'].includes(getFieldValue("frequency")) && (
+          <Form.Item wrapperCol={{ offset: 8, span: 16 }}>
+            <Row>
+              <Col span={8}>
+                <Form.Item label="Start date" labelCol={{ span: 24 }} wrapperCol={{ span: 24 }}>
+                  {getFieldDecorator('startDate', {
+                    initialValue: moment(),
+                    rules: [{ required: true, message: 'Please select your Start date!' }],
+                  })(
+                    <DatePicker format={dateFormat} />,
+                  )}
+                </Form.Item>
+              </Col>
+              <Col span={8}>
+                <Form.Item label="End date" labelCol={{ span: 24 }} wrapperCol={{ span: 24 }}>
+                  {getFieldDecorator('endDate', {
+                    initialValue: moment().add(1, 'days'),
+                    rules: [{ required: true, message: 'Please select your End date!' }],
+                  })(
+                    <DatePicker format={dateFormat} />,
+                  )}
+                </Form.Item>
+              </Col>
+              <Col span={8}>
+                <Form.Item label="Select time" labelCol={{ span: 24 }} wrapperCol={{ span: 24 }}>
+                  {getFieldDecorator('time', {
+                    initialValue: moment(),
+                    rules: [{ required: true, message: 'Please select your time!' }],
+                  })(
+                    <TimePicker format={timeFormat} />,
+                  )}
+                </Form.Item>
+              </Col>
+            </Row>
+          </Form.Item>
         )}
-        {getFieldValue("frequency") === 'e' && (
-          <div style={{ display: 'flex', justifyContent: 'flex-end' }}>
-            <Form.Item label="Date" labelCol={{ span: 24 }} wrapperCol={{ span: 24 }}>
-              {getFieldDecorator('date', {
-                rules: [{ required: true, message: 'Please select your Start date!' }],
-              })(
-                <DatePicker style={{ width: '150px' }} />,
-              )}
-            </Form.Item>
-            <Form.Item label="Schedule custom range" labelCol={{ span: 24 }} wrapperCol={{ span: 24 }}>
-              {getFieldDecorator('scheduleCustomRange', {
-                rules: [{ required: true, message: 'Please select your End date!' }],
-              })(
-                <DatePicker.RangePicker style={{ width: '150px' }} />,
-              )}
-            </Form.Item>
-            <Form.Item label="Schedule Time" labelCol={{ span: 24 }} wrapperCol={{ span: 24 }}>
-              {getFieldDecorator('time', {
-                rules: [{ required: true, message: 'Please select your time!' }],
-              })(
-                <TimePicker style={{ width: '150px' }} />,
-              )}
-            </Form.Item>
-          </div>
+        {getFieldValue("frequency") === 'Custom date' && (
+          <Form.Item wrapperCol={{ offset: 8, span: 16 }}>
+            <Row>
+              <Col span={8}>
+                <Form.Item label="Date" labelCol={{ span: 24 }} wrapperCol={{ span: 24 }}>
+                  {getFieldDecorator('date', {
+                    initialValue: moment(),
+                    rules: [{ required: true, message: 'Please select your Start date!' }],
+                  })(
+                    <DatePicker format={dateFormat} />,
+                  )}
+                </Form.Item>
+              </Col>
+              <Col span={8}>
+                <Form.Item label="Schedule custom range" labelCol={{ span: 24 }} wrapperCol={{ span: 24 }}>
+                  {getFieldDecorator('scheduleCustomRange', {
+                    initialValue: [moment(), moment()],
+                    rules: [{ required: true, message: 'Please select your End date!' }],
+                  })(
+                    <DatePicker.RangePicker format={dateFormat} />,
+                  )}
+                </Form.Item>
+              </Col>
+              <Col span={8}>
+                <Form.Item label="Schedule Time" labelCol={{ span: 24 }} wrapperCol={{ span: 24 }}>
+                  {getFieldDecorator('time', {
+                    initialValue: moment(),
+                    rules: [{ required: true, message: 'Please select your time!' }],
+                  })(
+                    <TimePicker format={timeFormat} />,
+                  )}
+                </Form.Item>
+              </Col>
+            </Row>
+          </Form.Item>
         )}
+        {getFieldValue('frequency') === 'Custom cron' && <><Form.Item wrapperCol={{ offset: 8 }}>
+          {getFieldDecorator("cronExpression", {
+            rules: [{ required: false }],
+          })(
+            <Alert 
+              message={
+                <b>
+                  {cronExpression.join(" ")}
+                </b>
+              }
+              type="info"
+            />
+          )}
+        </Form.Item>
         <Form.Item wrapperCol={{ offset: 8 }}>
           {getFieldDecorator("cronNarration", {
             rules: [{ required: false }],
@@ -158,7 +206,7 @@ const Schedule = (props) => {
               type="info"
             />
           )}
-        </Form.Item>
+        </Form.Item></>}
         <Form.Item label="Request method">
           {getFieldDecorator("requestMethod", {
             initialValue: 'Post',
@@ -176,7 +224,7 @@ const Schedule = (props) => {
         <Form.Item label="Auth required">
           {getFieldDecorator("authRequired", {
             initialValue: 'Yes',
-            rules: [{ required: true, message: "Please input your Password!" }],
+            rules: [{ required: true, message: "Please select any field!" }],
           })(
             <Select>
               {authRequired.map((itm) => (
@@ -189,10 +237,14 @@ const Schedule = (props) => {
         </Form.Item>
         <Form.Item label="Request parameter">
           {getFieldDecorator("requestParameter", {
-            rules: [{ required: true, message: "Please input your Password!" }],
-          })(<Input.TextArea placeholder="Url" disabled />)}
+            rules: [{ required: true, message: "Please input your Request parameter!" }],
+          })(<Input.TextArea placeholder="Request parameter" />)}
+        </Form.Item>
+        <Form.Item wrapperCol={{ offset: 8 }}>
+          <Button htmlType="submit" type="primary" >Save</Button>
         </Form.Item>
       </Form>
+      <br/>
     </div>
   );
 };
